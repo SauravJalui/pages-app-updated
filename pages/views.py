e2aa98c8d34f7d43bff7f11ccbc3b217aa0679d9
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-08 17:14:48 +0530

    updated requirements.txt and added docstrings

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
+    '''This class contains the data to be displayed on the home page'''
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
+    '''This class contains the data to be displayed on the about page'''
     template_name = 'about.html'
